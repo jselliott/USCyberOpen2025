5fa86851a5eff49b63ffc0d4c582b989b5994659
Jacob Elliott <jacob@jseis.me>
2025-06-18 15:26:36 -0400

    Fixing stuff

--- challenges/re/oasis/src/check_flag.c
+++
@@ -1,5 +0,0 @@
-#include <string.h>
-
-int check_flag(const char *input) {
-    return strcmp(input, "SVUSCG{h4ppy_hump_d4y}") == 0;
-}
--- challenges/re/viral/src/flag_check.c
+++
@@ -1,8 +0,0 @@
-#include <string.h>
-
-// dynamically resolved at runtime
-int (*my_strcmp)(const char *, const char *);
-
-int check_flag(const char *arg) {
-    return my_strcmp(arg, "SVUSCG{1_sh0uldnt_h4v3_tru5t3d_b1t_t0rr3nt}") == 0;
-}
